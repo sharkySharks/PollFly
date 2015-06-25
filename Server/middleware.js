5f495faabbd58f3dc4e7cd50c30cb8873777d371
Sharky <kaydenalthen@gmail.com>
2015-06-24 23:57:55 -0500

    code cleanup

--- Server/middleware.js
+++ Server/middleware.js
@@ -6,17 +6,4 @@ module.exports = function(app, express){
   app.use(bodyParser.json());
   app.use('/polls', polls);
   require('./polls/poll-routes.js')(polls);
-
-  // app.get('*', function(req, res, next){
-  //   var err = new Error();
-  //   err.status = 404;
-  //   next(err);
-  // });
-
-  // app.use(function(err, req, res, next){
-  //   if (err.status !== 404){
-  //     return next();
-  //   }
-  //   res.send(err.message || '** no unicorns here **');
-  // });
 };
--- Server/polls/poll-routes.js
+++ Server/polls/poll-routes.js
@@ -5,8 +5,4 @@ module.exports = function (polls){
   polls.post('/votes', pollFunctions.vote);
   polls.get('/:pollId', pollFunctions.getPoll);
   polls.get('/:pollId/result', pollFunctions.getPoll);
-
-  // polls.get('*', function(req, res){
-  //   res.send('No can do. 404-resource not found.', 404);
-  // });
 };
